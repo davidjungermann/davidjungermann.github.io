b220e46a9319216ccc0160e1712662a124dd3f33
DavidJungermann <dic15dju@student.lu.se>
2020-07-21 00:06:48 +0200

    added information

--- portfolio/src/components/Content.jsx
+++ portfolio/src/components/Content.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import experiences from "../assets/molecule.svg";
 import projects from "../assets/spaceship.svg";
 import skills from "../assets/skill.svg";
-import "../stylesheets/Content.css";
 
 class Content extends React.Component {
   render() {
@@ -10,15 +9,47 @@ class Content extends React.Component {
       <div class="container">
         <div class="row">
           <div class="col-sm">
-            <img className="skillsIcon" src={skills} height="60" width="60" alt="lightbulb"/>
+            <img
+              className="skillsIcon"
+              src={skills}
+              height="60"
+              width="60"
+              alt="lightbulb"
+            />
             <h2>Skills</h2>
+            <br></br>
+            <p>Programming</p>
+            <p>Web Development</p>
+            <p>API Development</p>
+            <p>Database Development</p>
+            <p>UX & UI design</p>
+            <p>Project Management</p>
           </div>
           <div class="col-sm">
-            <img className="expIcon" src={experiences} height="60" width="60" alt="molecule" />
+            <img
+              className="expIcon"
+              src={experiences}
+              height="60"
+              width="60"
+              alt="molecule"
+            />
             <h2>Experiences</h2>
+            <br></br>
+            <p>React.js | Angular</p>
+            <p>Typescript | Javascript</p>
+            <p>Java | .NET | Python</p>
+            <p>SQL Server | SQLite</p>
+            <p>Node.js | Webpack | Babel </p>
+            <p>Azure Devops | Docker</p>
           </div>
           <div class="col-sm">
-            <img className="projIcon" src={projects} height="60" width="60" alt="spaceship" />
+            <img
+              className="projIcon"
+              src={projects}
+              height="60"
+              width="60"
+              alt="spaceship"
+            />
             <h2>Projects</h2>
           </div>
         </div>
