53cd0c7509c6eafaa5165d1f3ab46bddeb8df5f8
DavidJungermann <dic15dju@student.lu.se>
2020-07-20 22:34:18 +0200

    added some styling

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import './App.css';
-import Header from "./components/Header"
+import React from "react";
+import "./App.css";
+import Header from "./components/Header";
+import { Helmet } from "react-helmet";
 
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <style>{"body { background-color: #7395AE; }"}</style>
+      </Helmet>
       <Header></Header>
     </div>
   );
--- portfolio/src/components/Header.jsx
+++ portfolio/src/components/Header.jsx
@@ -1,42 +1,45 @@
 import React from "react";
-import github from "../assets/github.svg"
-import linkedin from "../assets/linkedin.png"
-import profile from "../assets/image_profile.jpg"
+import github from "../assets/github.svg";
+import linkedin from "../assets/linkedin.png";
+import profile from "../assets/image_profile.jpg";
+import "../stylesheets/Header.css";
+import Jumbotron from "react-bootstrap/Jumbotron";
+
 class Header extends React.Component {
   render() {
     return (
-      <div className="jumbotron">
-        <div className="float-right">
-          <div>
-            <a href="https://github.com/davidjungermann" className="no-underline">
-              <img src={github} height="40" width="40" alt="Github"/>
-            </a>
-            <a
-              href="https://www.linkedin.com/in/david-jungermann-13a550162/"
-              className="no-underline"
-            >
-              <img src={linkedin} height="40" width="40" alt="LinkedIn"/>
-            </a>
-          </div>
-        </div>
-        
-        <div className="container">
-          <div className="row">
-            <div className="col-md-4 align-middle">
-              <img
-                src={profile}
-                height="250"
-                width="250"
-                className="rounded-circle float-left"
-                alt="Profile"
-              />
+      <div className="container">
+        <div className="jumbotron">
+          <div className="float-right">
+            <div>
+              <a href="https://github.com/davidjungermann">
+                <img src={github} height="40" width="40" alt="Github" />
+              </a>
+              <a href="https://www.linkedin.com/in/david-jungermann-13a550162/">
+                <img src={linkedin} height="40" width="40" alt="LinkedIn" />
+              </a>
             </div>
-            <div className="col-md-5 mt-5">
-              <h1>David Jungermann</h1>
-              <p>
-                Student Talent at Tetra Pak | 5th year MSc student | Information
-                and communication engineering technologies
-              </p>
+          </div>
+
+          <div className="container">
+            <div className="row">
+              <div className="col-md-4 align-middle">
+                <img
+                  src={profile}
+                  height="250"
+                  width="250"
+                  className="rounded-circle float-left"
+                  alt="Profile"
+                />
+              </div>
+              <div className="col-md-5 mt-5">
+                <h1>David Jungermann</h1>
+                <br></br>
+                <h5>
+                  5th year MSc student in Information and communication engineering technologies
+                </h5>
+                Student Talent at Tetra Pak
+              </div>
             </div>
           </div>
         </div>
