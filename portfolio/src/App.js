439931efca8434951e97410c826a47b9bb94fedc
DavidJungermann <dic15dju@student.lu.se>
2020-07-21 22:18:50 +0200

    restyled wip

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <Helmet>
-        <style>{"body { background-color: #f9f9f9; }"}</style>
+        <style>{"body { background-color: #D8DEE9; }"}</style>
       </Helmet>
       <Links></Links>
       <Header></Header>
--- portfolio/src/components/Header.jsx
+++ portfolio/src/components/Header.jsx
@@ -5,30 +5,27 @@ import "../stylesheets/Header.css";
 class Header extends React.Component {
   render() {
     return (
-      <div className="container">
-        <div className="jumbotron">
-          <div className="container">
-            <div className="row">
-              <div className="col-md-4 align-middle">
-                <img
-                  src={profile}
-                  height="250"
-                  width="250"
-                  className="rounded-circle float-left"
-                  alt="Profile"
-                />
-              </div>
-              <div className="col-md-5 mt-5">
-                <h1>David Jungermann</h1>
-                <br></br>
-                <h5>
-                  5th year MSc student in Information and communication engineering technologies
-                </h5>
-                Student Talent at Tetra Pak
-              </div>
+      <div className="jumbotron">
+          <div className="row">
+            <div className="col-md-4 align-middle">
+              <img
+                src={profile}
+                height="250"
+                width="250"
+                className="rounded-circle float-left"
+                alt="Profile"
+              />
+            </div>
+            <div className="col-md-5 mt-5">
+              <h1 className="header-text">David Jungermann</h1>
+              <br></br>
+              <h5 className="header-text">
+                5th year MSc student in Information and communication
+                engineering technologies at Lunds Tekniska Högskola
+              </h5>
+              <p className="header-text">Student Talent at Tetra Pak</p>
             </div>
           </div>
-        </div>
       </div>
     );
   }
