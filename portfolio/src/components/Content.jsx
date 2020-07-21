4a41624c04c8956e0d0f1fdcee6b2a483f1ee904
DavidJungermann <dic15dju@student.lu.se>
2020-07-21 18:02:25 +0200

    first draft of wip finished

--- portfolio/src/components/Content.jsx
+++ portfolio/src/components/Content.jsx
@@ -38,7 +38,7 @@ class Content extends React.Component {
             <p>React.js | Angular</p>
             <p>Typescript | Javascript</p>
             <p>Java | .NET | Python</p>
-            <p>SQL Server | SQLite</p>
+            <p>SQL Server | SQLite | Firebase</p>
             <p>Node.js | Webpack | Babel </p>
             <p>Azure Devops | Docker</p>
           </div>
@@ -51,6 +51,22 @@ class Content extends React.Component {
               alt="spaceship"
             />
             <h2>Projects</h2>
+            <br></br>
+            <p>
+              <a href="https://github.com/davidjungermann/voting-vg">
+                Vote counter. Built in React, uses ExcelJS and Firebase.
+              </a>
+            </p>
+            <p>
+              <a href="https://github.com/davidjungermann/EDAF90-project">
+                Forum mock up. Built in Angular, uses AngularFire.
+              </a>
+            </p>
+            <p>
+              <a href="https://github.com/davidjungermann/EDAF60">
+                Two projects that implement OOP principles. Built in Java.
+              </a>
+            </p>
           </div>
         </div>
       </div>
