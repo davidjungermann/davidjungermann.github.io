fb655194917386e11b62ddd8886c7863874edf74
DavidJungermann <dic15dju@student.lu.se>
2020-07-27 21:24:48 +0200

    test animate icon

--- portfolio/src/components/Start.jsx
+++ portfolio/src/components/Start.jsx
@@ -7,31 +7,29 @@ function Start() {
   const props = useSpring({
     opacity: 1,
     from: { opacity: 0 },
-    config: { duration: 2500 },
+    config: { duration: 2000 },
   });
   return (
     <div className="header">
-      <img
-        src={profile}
-        height="350"
-        width="350"
-        className="rounded-circle"
-        alt="Profile"
-      />
-      <div>
-        <div className="header-text">
-          <animated.h1 style={props}>David Jungermann</animated.h1>
-          <animated.h5 style={props}>
+      <animated.div style={props}>
+        <img
+          src={profile}
+          height="350"
+          width="350"
+          className="rounded-circle"
+          alt="Profile"
+        />
+        <div>
+          <div className="header-text">
+            <h1>David Jungermann</h1>
             <h5>5th year MSc student at LTH</h5>
-          </animated.h5>
-          <animated.h5 style={props}>
             <h5>
               Full stack developer in the Student Talent programme at Tetra Pak
             </h5>
-          </animated.h5>
+          </div>
+          <div></div>
         </div>
-        <div></div>
-      </div>
+      </animated.div>
     </div>
   );
 }
