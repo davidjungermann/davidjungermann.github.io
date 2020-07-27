c4f3917de26c157f439f8a744185b88a84ac2053
DavidJungermann <dic15dju@student.lu.se>
2020-07-27 22:26:25 +0200

    added animated scroll indicator

--- portfolio/src/components/Start.jsx
+++ portfolio/src/components/Start.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useSpring, animated } from "react-spring";
 import profile from "../assets/image_profile.jpg";
+import scroll from "../assets/arrow-down.svg";
 import "../stylesheets/Start.css";
 
 function Start() {
@@ -9,6 +10,13 @@ function Start() {
     from: { opacity: 0 },
     config: { duration: 2000 },
   });
+
+  const { x } = useSpring({
+    from: { x: 0 },
+    to: { x: 1 },
+    config: {mass: 1, tension: 500, friction: 10}
+  });
+
   return (
     <div className="header">
       <animated.div style={props}>
@@ -29,6 +37,18 @@ function Start() {
           </div>
           <div></div>
         </div>
+      </animated.div>
+      <animated.div
+        style={{
+          transform: x
+            .interpolate({
+              range: [0, 0.25, 0.35, 0.45, 0.55, 0.65, 0.75, 1],
+              output: [0, -10, 0, 10, 0, -10, 0, 10, 0]
+            })
+            .interpolate(x => `translate3d(0px, ${x}px, 0px)`)
+        }}
+      >
+        <img className="scroll" src={scroll} height="40" width="40" alt="scroll"></img>
       </animated.div>
     </div>
   );
