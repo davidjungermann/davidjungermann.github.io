f5a140c5a5baf7b2d46533a7718472072bbe98a1
DavidJungermann <dic15dju@student.lu.se>
2020-07-27 22:29:52 +0200

    removed fade in

--- portfolio/src/components/Start.jsx
+++ portfolio/src/components/Start.jsx
@@ -5,50 +5,48 @@ import scroll from "../assets/arrow-down.svg";
 import "../stylesheets/Start.css";
 
 function Start() {
-  const props = useSpring({
-    opacity: 1,
-    from: { opacity: 0 },
-    config: { duration: 2000 },
-  });
-
   const { x } = useSpring({
     from: { x: 0 },
     to: { x: 1 },
-    config: {mass: 1, tension: 500, friction: 10}
+    config: { mass: 1, tension: 500, friction: 10 },
   });
 
   return (
     <div className="header">
-      <animated.div style={props}>
-        <img
-          src={profile}
-          height="350"
-          width="350"
-          className="rounded-circle"
-          alt="Profile"
-        />
-        <div>
-          <div className="header-text">
-            <h1>David Jungermann</h1>
-            <h5>5th year MSc student at LTH</h5>
-            <h5>
-              Full stack developer in the Student Talent programme at Tetra Pak
-            </h5>
-          </div>
-          <div></div>
+      <img
+        src={profile}
+        height="350"
+        width="350"
+        className="rounded-circle"
+        alt="Profile"
+      />
+      <div>
+        <div className="header-text">
+          <h1>David Jungermann</h1>
+          <h5>5th year MSc student at LTH</h5>
+          <h5>
+            Full stack developer in the Student Talent programme at Tetra Pak
+          </h5>
         </div>
-      </animated.div>
+        <div></div>
+      </div>
       <animated.div
         style={{
           transform: x
             .interpolate({
               range: [0, 0.25, 0.35, 0.45, 0.55, 0.65, 0.75, 1],
-              output: [0, -10, 0, 10, 0, -10, 0, 10, 0]
+              output: [0, -10, 0, 10, 0, -10, 0, 10, 0],
             })
-            .interpolate(x => `translate3d(0px, ${x}px, 0px)`)
+            .interpolate((x) => `translate3d(0px, ${x}px, 0px)`),
         }}
       >
-        <img className="scroll" src={scroll} height="40" width="40" alt="scroll"></img>
+        <img
+          className="scroll"
+          src={scroll}
+          height="40"
+          width="40"
+          alt="scroll"
+        ></img>
       </animated.div>
     </div>
   );
