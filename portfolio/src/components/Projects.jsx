c29fe1709c206e6ef7a2a1b7baa5ee85225257f1
DavidJungermann <dic15dju@student.lu.se>
2020-08-28 15:08:43 +0200

    added webcards

--- portfolio/src/components/Projects.jsx
+++ portfolio/src/components/Projects.jsx
@@ -9,54 +9,122 @@ function Projects() {
         <div className="container">
           <div className="info-body">
             <h1 className="content-heading">Projects</h1>
-            Vote counter that can be used generate results from votings made in
-            Google Forms. Built in React, uses ExcelJS and Firebase.
-            <p>
-              <b>
-                <a href="https://github.com/davidjungermann/voting-vg">
-                  Source
-                </a>
-              </b>
-            </p>
-            Portfolio page, which happens to be this very page! Built in React
-            and hosted on Github Pages.
-            <p>
-              <b>
-                <a href="https://github.com/davidjungermann/davidjungermann.github.io">
-                  Source
-                </a>
-              </b>
-            </p>
-            Forum mock up. Built in Angular, uses AngularFire.
-            <p>
-              <b>
-                <a href="https://github.com/davidjungermann/EDAF90-project">
-                  Source
-                </a>
-              </b>
-            </p>
-            REST API with corresponding database that models production of
-            cookies. Built in Java, using Spark, and SQLite.
-            <p>
-              <b>
-                <a href="https://github.com/davidjungermann/EDAF75-Project">
-                  Source
-                </a>
-              </b>
-            </p>
-            Two projects that implement OOP principles. Built in Java.
-            <p>
-              <b>
-                <a href="https://github.com/davidjungermann/EDAF60">Source</a>
-              </b>
-            </p>
-            Mini-projects concerning web security, including payments,
-            electronic voting and cybersecurity. Written in Python.
-            <p>
-              <b>
-                <a href="https://github.com/davidjungermann/EITN41">Source</a>
-              </b>
-            </p>
+            <div className="row">
+              <div className="col-sm">
+                <div className="card">
+                  <div className="card-body">
+                    <h5 className="card-title">Voting system</h5>
+                    <p className="card-text">
+                      Application used for electronic voting. Used for
+                      generating results from votings made in Google Forms.
+                      Built in React, uses ExcelJS and Firebase.
+                    </p>
+                    <a
+                      href="https://github.com/davidjungermann/voting-vg"
+                      className="card-link"
+                    >
+                      Source
+                    </a>
+                    <a
+                      href="https://davidjungermann.com/voting-vg"
+                      className="card-link"
+                    >
+                      Website
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div className="col-sm">
+                <div className="card">
+                  <div className="card-body">
+                    <h5 className="card-title">Personal website</h5>
+                    <p className="card-text">
+                      Portfolio, which happens to be this very page! Built in
+                      React and hosted on Github Pages.
+                    </p>
+                    <a
+                      href="https://github.com/davidjungermann/davidjungermann.github.io"
+                      className="card-link"
+                    >
+                      Source
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div className="col-sm">
+                <div className="card">
+                  <div className="card-body">
+                    <h5 className="card-title">Message board forum</h5>
+                    <p className="card-text">
+                      Forum mock up. Built in Angular, uses AngularFire.
+                    </p>
+                    <a
+                      href="https://github.com/davidjungermann/EDAF90-project"
+                      className="card-link"
+                    >
+                      Source
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="col-sm">
+                <div className="card">
+                  <div className="card-body">
+                    <h5 className="card-title">
+                      API and database implementation
+                    </h5>
+                    <p className="card-text">
+                      REST API with corresponding database that models
+                      production of cookies. Built in Java, using Spark, and
+                      SQLite.
+                    </p>
+                    <a
+                      href="https://github.com/davidjungermann/EDAF75-Project"
+                      className="card-link"
+                    >
+                      Source
+                    </a>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-sm">
+                <div className="card">
+                  <div className="card-body">
+                    <h5 className="card-title">Object oriented programming</h5>
+                    <p className="card-text">
+                      Two projects that implement OOP principles. Built in Java.
+                    </p>
+                    <a
+                      href="https://github.com/davidjungermann/EDAF60"
+                      className="card-link"
+                    >
+                      Source
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div className="col-sm">
+                <div className="card">
+                  <div className="card-body">
+                    <h5 className="card-title">Web security</h5>
+                    <p className="card-text">
+                      Mini-projects concerning web security, including payments,
+                      electronic voting and cybersecurity. Written in Python.
+                    </p>
+                    <a
+                      href="https://github.com/davidjungermann/EITN41"
+                      className="card-link"
+                    >
+                      Source
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
