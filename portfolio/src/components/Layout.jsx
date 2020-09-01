ed0dfc4b5ef9dd6d027b1c98dbc0e0855b1946cd
DavidJungermann <dic15dju@student.lu.se>
2020-09-01 20:56:05 +0200

    removed nav

--- portfolio/src/components/Layout.jsx
+++ portfolio/src/components/Layout.jsx
@@ -12,7 +12,6 @@ export default function Layout() {
   return (
     <div className="">
       <Fullpage>
-        <FullpageNavigation />
           <FullPageSections>
             <Start />
             <About />
