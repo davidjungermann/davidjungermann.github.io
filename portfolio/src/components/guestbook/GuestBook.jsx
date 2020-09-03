8f81fd8d9a944c7fe2746471d48fff9f1f3474cb
DavidJungermann <dic15dju@student.lu.se>
2020-09-03 21:10:10 +0200

    changes

--- portfolio/src/components/guestbook/GuestBook.jsx
+++ portfolio/src/components/guestbook/GuestBook.jsx
@@ -10,20 +10,29 @@ function GuestBook(props) {
       <div className="content">
         <div className="container">
           <div className="info-body">
-            <div>
-              <h1>Guestbook</h1>
-              <h5 className="content-heading">
-                Write something nice, pretend it's 1999!
-              </h5>
-              <div className="row">
-                <div className="col-sm">
-                  <GuestBookForm></GuestBookForm>
+            {props.isDesktop ? (
+              <div>
+                <h1>Guestbook</h1>
+                <h5 className="content-heading">
+                  Write something nice, pretend it's 1999!
+                </h5>
+                <div className="row">
+                  <div className="col-sm">
+                    <GuestBookForm></GuestBookForm>
+                  </div>
+                  <div className="col-sm">
+                    <GuestBookLog></GuestBookLog>
+                  </div>
                 </div>
+              </div>
+            ) : (
+              <div>
+                <h1 className="content-heading">Guestbook</h1>
                 <div className="col-sm">
                   <GuestBookLog></GuestBookLog>
                 </div>
               </div>
-            </div>
+            )}
           </div>
         </div>
       </div>
