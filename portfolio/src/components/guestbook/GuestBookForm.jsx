df0e9d821e648fe14fb5bc1bf9bfa67c90289199
DavidJungermann <dic15dju@student.lu.se>
2020-09-04 08:00:07 +0200

    autocomplete off

--- portfolio/src/components/guestbook/GuestBookForm.jsx
+++ portfolio/src/components/guestbook/GuestBookForm.jsx
@@ -68,6 +68,7 @@ function GuestBookForm() {
           <Form.Group as={Col} md="8" controlId="validationCustom01">
             <Form.Control
               className="input-name"
+              autocomplete="off"
               size="lg"
               required
               type="text"
