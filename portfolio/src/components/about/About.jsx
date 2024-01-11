b5e473dace09c385ae21dd86fb7df97c1198ab36
David Jungermann <david.jungermann@gmail.com>
2024-01-11 17:45:10 +0100

    Updates content

--- portfolio/src/components/about/About.jsx
+++ portfolio/src/components/about/About.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import resume from '../../assets/Jungermann_David_Resume.pdf';
 import '../../stylesheets/Content.css';
 
 function About() {
@@ -8,46 +7,24 @@ function About() {
       <div className="container">
         <div className="info-body">
           <h1 className="content-heading">About me</h1>
+          <p>My name is David Jungermann, and I try to be creative, but I'm not.</p>
+
           <p>
-            My name is David Jungermann, and I'm a <b>Software Engineer</b>, with focus on{' '}
-            <b>fullstack development</b>. I graduated from Lunds Tekniska Högskola in the spring of
-            2021, with an MSc degree in{' '}
-            <b>Information and Communication engineering technologies</b>.
+            I make my living as a software engineer, currently working at Netlight in Stockholm. I
+            mainly work with topics related to cloud, devops and SRE. Big proponent of statically
+            typed languages and hard contracts.
           </p>
+
           <p>
-            As of August 2021, I'm working as an IT consultant at{' '}
-            <b>
-              <a href="https://netlight.com/">Netlight</a>
-            </b>{' '}
-            in Stockholm, Sweden.
+            I find joy in making developers happy - through automation, observability and data
+            driven decision making.
           </p>
+
           <p>
-            My master's thesis was written within Interaction Design at{' '}
-            <b>
-              <a href="https://homepal.se/">Homepal</a>
-            </b>
-            , with the title: <i>Optimizing the usability of REST API reference documentation</i>.
-            The master's thesis explores what features and properties should be included in a
-            reference documentation order to support the needs of the end users.
+            I'm a hobby hoarder, but outside improving developer experiences, I play music often,
+            but not well, both for myself and in a couple of bands. I like cooking, as well as
+            architecture and design - the non programming kind as well.
           </p>
-          <p>
-            In my spare time I make a lot of music, both individually and in bands. Furthermore, I
-            like skateboarding, hiking and hanging out with friends and family.
-          </p>
-          <p className="para-end">
-            For further information, check out my resume{' '}
-            <b>
-              {' '}
-              <a href={resume} download>
-                here.
-              </a>
-            </b>
-          </p>
-          Shoot me an{' '}
-          <b>
-            <a href="mailto:david.jungermann@gmail.com">email</a>
-          </b>
-          , or write in my guestbook below if you want to get in touch!
         </div>
       </div>
     </div>
--- portfolio/src/components/landing/StartDesktop.jsx
+++ portfolio/src/components/landing/StartDesktop.jsx
@@ -1,21 +1,31 @@
 import React, { useState } from 'react';
 import { AnimatedName } from './AnimatedName';
+import { Container, Row, Col } from 'react-bootstrap'; // Import Bootstrap components
 import '../../stylesheets/Header.css';
+import face from '../../assets/face.jpg';
 
 function StartDesktop() {
   const [textOpen, setTextOpen] = useState(true);
   return (
-    <div className="header-desktop">
-      <AnimatedName
-        className="header-text"
-        open={textOpen}
-        onClick={() => setTextOpen((state) => !state)}
-      >
-        <h1>David Jungermann</h1>
-        <h5>Software Engineer</h5>
-        <h5>IT Consultant at Netlight</h5>
-      </AnimatedName>
-    </div>
+    <Container className="header-desktop">
+      <Row>
+        <Col xs={12} md={6} className="my-auto">
+          {' '}
+          {/* Adjust column sizes as needed */}
+          <img src={face} alt="picture of me" className="face img-fluid" />
+        </Col>
+        <Col xs={12} md={6} className="my-auto">
+          <AnimatedName
+            className="header-text"
+            open={textOpen}
+            onClick={() => setTextOpen((state) => !state)}
+          >
+            <h1>Hej!</h1>
+            <h5>I'm David - a software engineer</h5>
+          </AnimatedName>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
